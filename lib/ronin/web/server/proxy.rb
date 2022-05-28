3efd19c2b3bdedb5d77a0cdb6ab99994ca0327ec
Postmodern <postmodern.mod3@gmail.com>
2022-05-27 22:24:57 -0700

    Use a standalone example for the proxy.

--- lib/ronin/web/server/proxy.rb
+++ lib/ronin/web/server/proxy.rb
@@ -33,19 +33,24 @@ module Ronin
       #
       # A Rack application for proxying requests.
       #
-      #     server.route '/forum/', Proxy.new do |proxy|
+      #     require 'ronin/web/server/proxy'
+      #     
+      #     log = File.new('log.txt','w+')
+      #     
+      #     proxy = Ronin::Web::Server::Proxy.new do |proxy|
       #       proxy.on_request do |request|
-      #         puts request.url
-      #       end
-      #
-      #       proxy.on_response do |response|
-      #         response.headers.each do |name,value|
-      #           puts "#{name}: #{value}"
-      #         end
-      #
-      #         puts response.body
-      #       end
-      #     end
+      #         log.puts "[#{request.ip} -> #{request.host_with_port}] #{request.request_method} #{request.url}"
+      #     
+      #         request.headers.each do |name,value|
+      #           log.puts "#{name}: #{value}"
+      #         end  
+      #     
+      #         log.puts request.params.inspect
+      #         log.flush
+      #       end  
+      #     end  
+      #     
+      #     proxy.run!
       #
       # @api semipublic
       #
