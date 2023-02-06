49d037f9f526c9891c2c2fc23c58a32d5f4f4620
Postmodern <postmodern.mod3@gmail.com>
2023-02-06 15:50:08 -0800

    Removed duplicate specs for `Ronin::Web::Server::Base`.

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,15 +1,10 @@
 require 'spec_helper'
 require 'ronin/web/server/base'
 
-require 'classes/test_app'
 require 'helpers/rack_app'
 
 describe Ronin::Web::Server::Base do
   include Helpers::Web::RackApp
-
-  before(:all) do
-    self.app = TestApp
-  end
 
   describe "DEFAULT_HOST" do
     it "must equal '0.0.0.0'" do
@@ -33,63 +28,19 @@ describe Ronin::Web::Server::Base do
     end
   end
 
-  it "must still bind blocks to paths" do
-    get '/tests/get'
-
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('block tested')
-  end
-
-  it "must bind a block to a path for all request types" do
-    post '/tests/any'
-
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('any tested')
-  end
-
-  it "must have a default response" do
-    get '/totally/non/existant/path'
-
-    expect(last_response).not_to be_ok
-    expect(last_response.body).to be_empty
-  end
-
-  it "must allow for defining custom responses" do
-    TestApp.default do
-      halt 404, 'nothing to see here'
+  describe "not_found" do
+    module TestBaseServer
+      class TestNotFound < Ronin::Web::Server::Base
+      end
     end
 
-    get '/whats/here'
+    let(:app) { TestBaseServer::TestNotFound }
 
-    expect(last_response).not_to be_ok
-    expect(last_response.body).to eq('nothing to see here')
-  end
+    it "must default to returning an empty 404 response" do
+      get '/does/not/exist'
 
-  it "must map paths to sub-apps" do
-    get '/tests/subapp/'
-
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('SubApp')
-  end
-
-  it "must not modify the path_info as it maps paths to sub-apps" do
-    get '/tests/subapp/hello'
-
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('SubApp greets you')
-  end
-
-  it "must host static content from public directories" do
-    get '/static1.txt'
-
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq("Static file1.\n")
-  end
-
-  it "must host static content from multiple public directories" do
-    get '/static2.txt'
-
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq("Static file2.\n")
+      expect(last_response.status).to be(404)
+      expect(last_response.body).to eq('')
+    end
   end
 end
