c2cee3b309198735ef90a0b383a5d259fb86a3d7
Postmodern <postmodern.mod3@gmail.com>
2026-01-12 06:41:57 -0800

    Ensure all test requests have the same `Host` header as the test Rack app.

--- spec/helpers/rack_app.rb
+++ spec/helpers/rack_app.rb
@@ -5,11 +5,39 @@ module Helpers
     module RackApp
       include Rack::Test::Methods
 
+      DEFAULT_HEADERS = {
+        'HTTP_HOST' => 'localhost'
+      }
+
       attr_reader :app
 
       def app=(server)
         @app = server
         @app.set :environment, :test
+      end
+
+      def get(path,params={},headers={})
+        super(path,params,DEFAULT_HEADERS.merge(headers))
+      end
+
+      def post(path,params={},headers={})
+        super(path,params,DEFAULT_HEADERS.merge(headers))
+      end
+
+      def put(path,params={},headers={})
+        super(path,params,DEFAULT_HEADERS.merge(headers))
+      end
+
+      def patch(path,params={},headers={})
+        super(path,params,DEFAULT_HEADERS.merge(headers))
+      end
+
+      def delete(path,params={},headers={})
+        super(path,params,DEFAULT_HEADERS.merge(headers))
+      end
+
+      def options(path,params={},headers={})
+        super(path,params,DEFAULT_HEADERS.merge(headers))
       end
     end
   end
